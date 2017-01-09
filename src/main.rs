3c1495943032d29465f4d7705708cc8d1a99eb44
Bryan E. Barnhart <bryan.e.barnhart@gmail.com>
2017-01-09 00:47:37 -0700

    changed run to chain option functions together, and added missing Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,8 @@ mod machine;
 type Result<T> = std::result::Result<T, Box<Error + Send + Sync>>;
 
 
-fn run(file: String) -> Result<i32> {
+fn run_exec(exec: Chunk, blorb: BlorbCursor<File>) -> Result<i32> {
     use blorb::Chunk::*;
-
-    let (blorb, exec) = File::open(file)
-        .and_then(|rom| BlorbCursor::from_file(rom))
-        .and_then(|mut blorb| blorb.load_resource(Usage::Exec, 0)
-            .map(|chunk| (blorb, chunk)))?;
-
     match exec {
         Adrift{code} => println!("got code!"),
         AdvSys{code} => println!("got code!"),
@@ -40,28 +34,31 @@ fn run(file: String) -> Result<i32> {
         ZCode{code} => println!("got code!"),
         Unknown{..} => println!("unrecognized chunk"),
         _ => println!("non-executable chunk found"),
-    }
+    };
     Ok(0)
+}
+
+fn run(file: String) -> Result<i32> {
+    File::open(file)
+        .and_then(|rom| BlorbCursor::from_file(rom))
+        .and_then(|mut blorb| blorb.load_resource(Usage::Exec, 0)
+            .map(|chunk| (chunk, blorb)))
+        .map_err(|err| err.into())
+        .and_then(|(chunk, blorb)| run_exec(chunk, blorb))
 }
 
 
 fn main() {
-    match args().nth(1) {
-        Some(file) => {
-            match run(file) {
-                Err(ref err) => {
-                    println!("error occured: {}", err);
-                    std::process::exit(1)
-                },
-                Ok(ret) => {
-                    println!("finished running");
-                    std::process::exit(ret)
-                },
-            }
+    match args().nth(1)
+            .ok_or_else(||"no file provided".into())
+            .and_then(run) {
+        Ok(ret) => {
+            println!("finished running");
+            std::process::exit(ret)
         },
-        None => {
-            println!("no file provided");
+        Err(ref err) => {
+            println!("error occured: {}", err);
             std::process::exit(1)
-        }
+        },
     }
 }
